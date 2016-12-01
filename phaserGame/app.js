c17cf69878b1f04405a470e03cc569c98b930924
POKEMON-PO-88 <christian.lamotte888@gmail.com>
2016-12-01 16:49:13 +0800

    added collision

--- phaserGame/app.js
+++ phaserGame/app.js
@@ -54,11 +54,31 @@ function create() {
 
 	// Set up keyboard events
 	cursors = game.input.keyboard.createCursorKeys();
-}
+	}
 
-function update() {
-	
+	function update() {
+	game.pyhsics.arcade.collide(player, platforms);
+	game.pyhsics.arcade.collide(enemy1, platforms);
+
+	player.body.velocity.x = 0;
+
+
+	if (cursors.left.isDown) {
+	player.body.velocity.x= -150;
+	player.animations.play("left");
+	}else if (cursors.right.isDown) {
+	player.body.velocity.x= +150;
+	player.animations.play("right");	
+	}else {
+	player.animations.stop();
+	player.frame = 4;
+	}
+
+	if(cursors.up.isDown && player.body.touching.down)
+	{
+		player.body.velocity.y = -350;
+	}
 
 
 
-}
+
